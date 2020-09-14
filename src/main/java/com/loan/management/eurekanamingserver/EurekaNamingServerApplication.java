9f3e42f652d5c0d1f8ac8de323ee8e54b407795b
unknown <ctsjavauser283@iiht.tech>
2020-09-14 13:29:08 +0530

    package changed

--- src/main/java/com/loan/management/eurekanamingserver/EurekaNamingServerApplication.java
+++ src/main/java/com/loan/management/eurekanamingserver/EurekaNamingServerApplication.java
@@ -1,4 +1,4 @@
-package com.scb.eurekanamingserver;
+package com.loan.management.eurekanamingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
